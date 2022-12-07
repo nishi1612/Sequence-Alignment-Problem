63a9fae5b84a91bf25a1e7008091ccfc7446081b
Purvil Mehta <purvilmehta06@gmail.com>
2022-12-07 02:05:57 -0800

    Fixes memory analysis difference

--- Analysis/graphPlotting.py
+++ Analysis/graphPlotting.py
@@ -1,33 +1,57 @@
 import matplotlib.pyplot as plt
+import os
+import re
 
 def readFile(f):
-    len = []
-    memory = []
-    time = []
     k = 0
+    l = 0
+    mem = 0
+    time = 0
     lines = f.readlines()
     for line in lines:
-
         if (line[-1] == '\n'):
             line = line[:-1]
-
-        words = line.split(',')
-        len.append(int(words[0]))
-        memory.append(float(words[2]))
-        time.append(float(words[1]))
+        if (k > 0 and k < 3): 
+            l += len(line.replace('_', ''))
+        elif (k == 3):
+            time = float(line)
+        elif (k == 4):
+            mem = float(line)
         k += 1
 
-    return len, memory, time
+    return l, mem, time
 
+basicMemory = []
+basicTime = []
+efficientMemory = []
+efficientTime = []
+length = []
 
-fb = open("basic_output.txt", 'r')
-fe = open("efficient_output.txt", 'r')
-len, basicMemory, basicTime = readFile(fb)
-len, efficientMemory, efficientTime = readFile(fe)
+fb = open("basic_output.csv", 'w')
+fe = open("efficient_output.csv", 'w')
+
+x = os.listdir("./datapoints_output_basic")
+x.sort(key=lambda f: int(re.sub('\D', '', f)))
+for file in x:
+    f = open("./datapoints_output_basic/" + file, 'r')
+    l, bm, bt = readFile(f)
+    basicMemory.append(bm)
+    basicTime.append(bt)
+    length.append(l)
+    fb.write(str(l) + "," + str(bm) + "," + str(bt) + '\n')
+
+x = os.listdir("./datapoints_output_efficient")
+x.sort(key=lambda f: int(re.sub('\D', '', f)))
+for file in x:
+    f = open("./datapoints_output_efficient/" + file, 'r')
+    l, em, et = readFile(f)
+    efficientMemory.append(em)
+    efficientTime.append(et)
+    fe.write(str(l) + "," + str(em) + "," + str(et) + '\n')
 
 fig1 = plt.figure(figsize=(10,6))
-plt.plot(len, efficientTime)
-plt.plot(len, basicTime)
+plt.plot(length, efficientTime)
+plt.plot(length, basicTime)
 plt.legend(['Efficient algorithm time consumed', 'Basic algorithm time consumed'])
 plt.xlabel('Total length of string (m+n)')
 plt.ylabel('Time consumed (ms)')
@@ -36,8 +60,8 @@ plt.grid()
 plt.savefig('timeComparision.png')
 
 fig2 = plt.figure(figsize=(10,6))
-plt.plot(len, efficientMemory)
-plt.plot(len, basicMemory)
+plt.plot(length, efficientMemory)
+plt.plot(length, basicMemory)
 plt.legend(['Efficient algorithm memory usage', 'Basic algorithm memory usage'])
 plt.xlabel('Total length of string (m+n)')
 plt.ylabel('Memory usage (KB)')
--- Analysis/graphPlotting.sh
+++ Analysis/graphPlotting.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+mkdir -p datapoints_output_basic
+mkdir -p datapoints_output_efficient
+
 for FILE in `ls datapoints/| sort -V`; do 
-    python3 basicAnalysis.py datapoints/$(basename ${FILE});
-    python3 efficientAnalysis.py datapoints/$(basename ${FILE});
+    python3 ../basic_3.py datapoints/$(basename ${FILE}) datapoints_output_basic/$(basename ${FILE});
+    python3 ../efficient_3.py datapoints/$(basename ${FILE}) datapoints_output_efficient/$(basename ${FILE});
+    echo "Completed Processing: ${FILE}" 
 done
 
 python3 graphPlotting.py
